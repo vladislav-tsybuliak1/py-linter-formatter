04259d7bcdb0da561cc45d6fcc5a6c01c311956b
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-07-17 11:52:40 +0300

    Fix format_linter_report

--- app/main.py
+++ app/main.py
@@ -18,5 +18,4 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(file, errors)
-            for file, errors
-            in linter_report.items()]
+            for file, errors in linter_report.items()]
